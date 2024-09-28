5223abb9734ccd082de78cc188547b1945f37414
Wassim Rahali <wassimrahali40@gmail.com>
2024-09-28 01:29:13 +0100

    SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("T"),
-      ),
-      body: Column(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Title',
+            style: TextStyle(
+                color:Colors.white),
+          ),
+          backgroundColor: Colors.amber
+        ),
+          body:SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ),
+                Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.purple
+                ),
+                Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.purple
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+              ],
+            )
+          )
 
-        children: [
-          Text("j"),
-          Text("j"),
-          Text("j"),
-          Container()
-        ],
-      )
-    ));
+            ),
+      );
 
   }
 }
