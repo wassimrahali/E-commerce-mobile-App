aa22c7fb10f2f2ac15e8dda8973c2cd546ac69ba
Wassim Rahali <wassimrahali40@gmail.com>
2024-10-02 12:36:41 +0100

    Categories Section

---
+++ lib/home.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Container(
+              child: ListView(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        fillColor: Colors.grey[200],
+                        filled: true,
+                        hintText: "Search",
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        prefixIcon: const Icon(Icons.search),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Icon(
+                      Icons.menu,
+                      size: 40,
+                    ),
+                  )
+                ],
+              ),
+              Container(height: 30),
+              const Text(
+                "Categories",
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(100)),
+                          child: const Icon(
+                            Icons.laptop,
+                            size: 40,
+                          ),
+                        ),
+                        Container(height: 8),
+                        const Text(
+                          'Laptop',
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(100)),
+                          child: const Icon(
+                            Icons.phone_android,
+                            size: 40,
+                          ),
+                        ),
+                        Container(height: 8),
+                        const Text(
+                          'Mobile',
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(100)),
+                          child: const Icon(
+                            Icons.local_offer_rounded,
+                            size: 40,
+                          ),
+                        ),
+                        Container(height: 8),
+                        const Text(
+                          'Clothes',
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(100)),
+                          child: const Icon(
+                            Icons.electric_bolt_rounded,
+                            size: 40,
+                          ),
+                        ),
+                        Container(height: 8),
+                        const Text(
+                          'Electronics',
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hey/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,125 +11,17 @@ class MyApp extends StatefulWidget {
 
   @override
   State<MyApp> createState() => _MyAppState();
+
 }
 
 
 class _MyAppState extends State<MyApp> {
-  GlobalKey<FormState> field = GlobalKey<FormState>();
-  GlobalKey<FormState>_field = GlobalKey<FormState>();
-  void _validator(){
-    if(field.currentState!.validate()){
-      print("form is valid");
-    }else{
-      print("form isnot valid");
-    }
-  }
-  void _verifta3nam(){
-    if(_field.currentState!.validate()){
-      print('ezab');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        drawer: Drawer(
-          backgroundColor: Colors.blue,
-          child: ListView(
-            children: [
-              ListTile(
-            title: const Text('Item 1'),
-            onTap: () {
-              // Update the state of the app.
-              // ...
-            },
-          ),
-              ListTile(
-                title: const Text('Item 1'),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              ListTile(
-                title: const Text('Item 1'),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-
-            ],
-          ),
-        ),
-        appBar: AppBar(
-          leading: Builder(builder:(context){
-            return IconButton(
-              icon:const Icon(Icons.account_balance),onPressed: (){
-              Scaffold.of(context).openDrawer();
-            }, );
-
-          }),
-          shadowColor: Colors.grey,
-          title: Text('Home',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w900,),),
-          backgroundColor: Colors.red,
-          centerTitle: true,
-          elevation: 20,
-          actions: [
-            Icon(Icons.next_plan,color: Colors.white,)
-          ],
-
-        ),
-
-
-        body: Container(
-          child: Column(
-            children: [
-              Container(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(18.0),
-                      child: Form(
-                          key: field,
-                          child: TextFormField(
-                            validator: (value) {
-                              if(value!.isEmpty){
-                                return 'please correct it max 8';
-                              }
-                              return null;
-                            },
-                          )
-                      ),
-                    ),
-                    MaterialButton(onPressed: _validator,
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      child: Text('data'),)
-                  ],
-                ),
-              ),
-              Container(
-                child:Form(
-                  key: _field,
-                  child:TextFormField(
-                  validator: (value) {
-                    if(value!.length<7){
-                      return 'ezibb zab zebi';
-                    }
-                    return null ;
-                  },
-                ),
-                ),
-              ),
-              MaterialButton(onPressed: _verifta3nam,
-              child: Text('Nam'),)
-            ],
-          )
-        ),
-      ),
+     home: Home() ,
     );
   }
 }
